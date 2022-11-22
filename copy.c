460d53f5c707141f3a1eb09f09dcb9eb8f203f2f
Your Name <you@example.com>
2022-11-22 15:25:59 +0200

    updated copy.c

--- copy.c
+++ copy.c
@@ -4,6 +4,14 @@
 
 #include <stdio.h>
 #include <stdlib.h> // For exit()
+#include <sys/stat.h>
+#include <fcntl.h>
+#include<limits.h>
+#include <errno.h>
+// #include<string.h>
+// #include <unistd.h>
+#define BUF_SIZE 8192
+
 
 
 /**
@@ -12,38 +20,88 @@
  * 2. /.copy <copy_from_destination> <copy_to_destination>
  */
 
-/**Copy function Copy between two files.
 
- * @param fp1 - Pointer to 1st file -> Copy from destination
- * @param fp2 - Pointer to 2nd file -> Copy to destination
- *
- * Source of information
- * https://www.geeksforgeeks.org/c-program-copy-contents-one-file-another-file/
- */
+int copyToFile(int fd_src, int fd_dest)
+{
+    ssize_t in, out;
+    char buf[BUF_SIZE];
+        while (1) {
+            in = read(fd_src, buf, BUF_SIZE);
+            if (in <= 0) break;
+            out = write(fd_dest, buf, in);
+            if (out <= 0) break;
+        }
+    printf("file is copied/n");
+
+    return 0;
+}
 
 
 int main(int argc, char **argv)
 {
-    //Open 2 input files
-    FILE *fptr1 = fopen(argv[2], "r");
-    FILE *fptr2 = fopen(argv[3], "w");;
+    if (argc != 3 && argc !=4 )
+    {
+        printf("Usage : copy <file1> <file2>");
+        exit(1);
 
-    //Check for valid files
-    if (!fptr1 || !fptr2)
-    {
-        printf("Cannot open files \n");
-        exit(0);
     }
-    // Read contents from file and copy it to the 2nd file
-    char c = fgetc(fptr1);
-    while (c != EOF)
+    char* from = argv[1];
+    char* to = argv[2];
+    int fd_src, fd_dest;
+
+    if ((fd_src = open(from ,O_RDONLY)) < 0)
     {
-        fputc(c, fptr2);
-        c = fgetc(fptr1);
+        perror("Error when opening the from file!");
+        exit(1);
     }
-    printf("file is copied");
-    // Close files
-    fclose(fptr1);
-    fclose(fptr2);
+    if ((fd_dest = open(to ,O_RDONLY | O_WRONLY | O_TRUNC | O_WRONLY|O_CREAT , 0666)) < 0)
+    {
+        perror("Error when opening the to file!");
+        exit(1);
+    }
+
+    if (argc == 3) // Three arguments -> regular copy
+    {
+        copyToFile(fd_src, fd_dest);
+        return 0;
+    }
+
+    struct stat buffer;
+    int status;
+
+    status = lstat(from, &buffer);
+    if (status == -1)
+    {
+        copyToFile(fd_src, fd_dest);
+        return 0;
+    }
+
+    char linkBuffer[PATH_MAX];
+    ssize_t nbytes = readlink(argv[1], linkBuffer, sizeof(linkBuffer));
+    if (nbytes == -1) {
+        copyToFile(fd_src, fd_dest);
+        return 0;
+    }
+    ssize_t writtenBytes;
+    writtenBytes = write(fd_dest, linkBuffer, nbytes);
+    if (writtenBytes < 0)
+        {
+            printf("this is the error\n");
+            if (errno == EDQUOT)
+            {
+                printf("ERROR: out of quota.\n");
+            }
+            else if (errno == ENOSPC)
+            {
+                printf("ERROR: not enough disk space.\n");
+            }
+        }
+
+    else printf("file is copied/n");
+
+    close(fd_src); 
+    close(fd_dest);
+
+
     return 0;
 }
